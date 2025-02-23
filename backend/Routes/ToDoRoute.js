ec1d197caadc773476bec10b54c127054c438011
Thitina Begashaw <thitina.t@abulkhase.org>
2025-02-23 13:03:05 -0800

    Add create , update , delete and get features

--- backend/Routes/ToDoRoute.js
+++ backend/Routes/ToDoRoute.js
@@ -1,7 +1,10 @@
-import createToDoList from "../controllers/ToDoController.js";
+import {createToDoList , updateToDoList , getToDoList , deleteToDoList} from "../controllers/ToDoController.js";
 import express from "express";
 const router = express.Router();
 
 router.post('/' , createToDoList)
+router.get('/' , getToDoList)
+router.patch('/:id' , updateToDoList)
+router.delete('/:id' , deleteToDoList)
 
 export default router;
